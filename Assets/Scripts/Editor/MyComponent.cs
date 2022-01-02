0b0fb69df97b766cba9136029515ccb7c30d320a
riekure <s2darl0uvgia24@gmail.com>
2022-01-02 16:28:19 +0900

    空ファイルに変更

--- Assets/Scripts/Editor/MyComponent.cs
+++ Assets/Scripts/Editor/MyComponent.cs
@@ -2,33 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyComponentAttibute : PropertyAttribute { }
-
 public class MyComponent : MonoBehaviour
 {
-
-	public int hoge;
-
-	[SerializeField, MyComponentAttibute]
-	private float editorPropertyDrawer;
-
-	[SerializeField]
-	private ItemData.Item item;
-
-	[Header("Built'in PropertyDrawer(Attribute)")]
-	[SerializeField, Range(0.0f, 10.0f)]
-	private float range;
-
-	[SerializeField, Min(0.5f)]
-	private float min;
-
-	[Space(30f)]
-	[SerializeField, Multiline(3)]
-	private string multiline;
-
-	[SerializeField, TextArea(2, 3)]
-	private string textarea;
-
-	[SerializeField, Tooltip("This is tooltip")]
-	private int tooltip;
 }
